40f2a0e02c73e55115f2ed085500588691427d87
prakharssingh <105123133+prakharssingh@users.noreply.github.com>
2025-01-15 01:44:45 +0530

    Update Find the Prefix Common Array of Two Arrays.java

--- src/Find the Prefix Common Array of Two Arrays.java
+++ src/Find the Prefix Common Array of Two Arrays.java
@@ -1,4 +1,5 @@
-    class Solution {
+// optimized
+class Solution {
     public int[] findThePrefixCommonArray(int[] a, int[] b) {
         int[] ans = new int[a.length];
         HashSet<Integer> seen = new HashSet<>();
@@ -15,6 +16,32 @@
             }
             else{
                 seen.add(b[i]);
+            }
+            ans[i]= count;
+        }
+        return ans;
+    }
+}
+// bruteforce
+class Solution {
+    public int[] findThePrefixCommonArray(int[] a, int[] b) {
+        int[] ans = new int[a.length];
+        HashMap<Integer, Integer> amap = new HashMap<>();
+        HashMap<Integer, Integer> bmap = new HashMap<>();
+        int count = 0;
+        for(int i = 0; i<a.length ; i++){
+            amap.put(a[i],1);
+            bmap.put(b[i],1);
+            if(a[i]==b[i]){
+                count++;
+                ans[i]= count;
+                continue;
+            }
+            if(bmap.containsKey(a[i])){
+                count++;
+            }
+            if(amap.containsKey(b[i])){
+                count++;
             }
             ans[i]= count;
         }
